8b96c2e0e7eb891286b555f5e3a87a634ac9115b
mjpritchard <matt.pritchard@stfc.ac.uk>
2019-04-30 17:07:56 +0100

    added custom filter for de_b'ing parsed feeds

--- apptheme_mezzanine/templatetags/apptheme_mezzanine_tags.py
+++ apptheme_mezzanine/templatetags/apptheme_mezzanine_tags.py
@@ -1,7 +1,14 @@
 from django import template
+from django.template.defaultfilters import stringfilter
 
 register = template.Library()
 
 @register.filter
 def portfolioitem_in_category(things, category):
     return things.filter(categories__in=[category,])
+
+@register.filter
+@stringfilter
+def de_b(value):
+    ''' If string value is prefixed as a bytecode (b'somestring', remove the wrapping b'')'''
+    return str(value.encode('utf-8'))[2:-1]
