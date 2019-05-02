4ecdc3c0937f472e573ca085f9db4b1d812193b3
mjpritchard <matt.pritchard@stfc.ac.uk>
2019-05-02 11:06:29 +0100

    added datemod, dateage filters

--- apptheme_mezzanine/templatetags/apptheme_mezzanine_tags.py
+++ apptheme_mezzanine/templatetags/apptheme_mezzanine_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from django.template.defaultfilters import stringfilter
+from datetime import datetime
 
 register = template.Library()
 
@@ -11,8 +12,23 @@ def portfolioitem_in_category(things, category):
 @stringfilter
 def de_b(value):
     ''' If string value is prefixed as a bytecode (b'somestring', remove the wrapping b'')'''
-    print value
     if value.startswith("b'"):
         return str(value.encode('utf-8'))[2:-1]
     else:
         return value
+
+@register.filter(expects_localtime=True)
+def datemod(value):
+    '''manually truncate timezone (last 6 chars) off a string time value,
+     then parse as datetime & return it'''
+    datestr = value[:-6]
+    print "datestr", datestr
+    newdate = datetime.strptime(datestr,'%a, %d %b %Y %H:%M:%S')
+    print "newdate: ", newdate
+    return newdate
+
+@register.filter(expects_localtime=True)
+def dateage(value):
+    '''return difference of a datetime versus now, in days'''
+    deltadays = datetime.now() - value
+    return deltadays.days
